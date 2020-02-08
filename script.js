ecaa8b20895993b68e210be433b24bfdf768b65d
Samantha Simmons <sammysimmons274@gmail.com>
2020-02-08 13:32:07 +0000

    file update

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-$("button").click(function() {
-    $(this).children("p").slideToggle(‘slow’);
+$(document).ready(function() {
+    //sets <a> element within paragraph  to yellow  
+    $("p").click(function() {
+        $(this).children("a").css("background-color", "yellow");
+        /* returns all the <a> child elements that are 
+		within this paragraph*/
+    });
+
+    // Toggle the visibility of the paragraph when a button is clicked 
+    $("button").click(function() {
+        $(this).next().slideToggle('slow');
+    });
 });
